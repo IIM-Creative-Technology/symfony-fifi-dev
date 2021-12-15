5a4c54bbbc5baa69405f446ae5313ac65b5a938a
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2021-12-15 22:38:03 +0100

    styles & mission route

--- src/Controller/MissionController.php
+++ src/Controller/MissionController.php
@@ -2,19 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Mission;
 use App\Repository\MissionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MissionController extends AbstractController
 {
+    private $missionRepository;
+    public function __construct(EntityManagerInterface $objectManager)
+    {
+        $this->missionRepository = $objectManager->getRepository(Mission::class);
+    }
+
+
     /**
-     * @Route("/missions", name="missions")
+     * @Route("/missions", name="app_missions")
      */
     public function index(MissionRepository $missionRepository): Response
     {
         $missions = $missionRepository->findAll();
         return $this->render('mission/index.html.twig', compact('missions'));
+    }
+
+    /**
+     * @Route ("/missions/{id<[0-9]+>}", name= "app_mission_show")
+     */
+    public function show(Mission $mission): Response
+    {
+        dd($mission);
     }
 }
