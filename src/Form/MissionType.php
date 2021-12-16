0686107a74d2b97f652768fc6cb57f319f470327
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2021-12-16 12:15:05 +0100

    create mission form update

--- src/Form/MissionType.php
+++ src/Form/MissionType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Mission;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -17,9 +19,23 @@ class MissionType extends AbstractType
         $builder
             ->add('Name', TextType::class)
             ->add('description', TextareaType::class)
-            ->add('priority')
-            ->add('execute_date')
-            ->add('status')
+            ->add('priority', ChoiceType::class, [
+                'choices'  => [
+                    'low' => 'low',
+                    'medium' => 'medium',
+                    'high' => 'high',
+
+                ]])
+            ->add('execute_date', DateType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('status', ChoiceType::class, [
+                'choices'  => [
+                    'low' => 'low',
+                    'medium' => 'medium',
+                    'high' => 'high',
+
+                ]])
             ->add('submit', SubmitType::class, ['label' =>  'Create'])
 
         ;
