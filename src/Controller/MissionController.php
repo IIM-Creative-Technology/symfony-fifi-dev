4935253aa453e72c627396063592fc80c775fc61
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2021-12-16 12:05:56 +0100

    create mission form

--- src/Controller/MissionController.php
+++ src/Controller/MissionController.php
@@ -3,15 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\Mission;
+use App\Form\MissionType;
 use App\Repository\MissionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MissionController extends AbstractController
 {
 
+    private $missionRepository;
+    public function __construct(EntityManagerInterface $objectManager)
+    {
+        $this->missionRepository = $objectManager->getRepository(Mission::class);
+    }
 
     /**
      * @Route("/missions", name="app_missions")
@@ -26,12 +33,27 @@ class MissionController extends AbstractController
 
     /**
      * @Route("/missions/create", name="app_missions_create")
+     * @param Request $request
+     * @return Response
      */
-    public function create(): Response
+    public function create(Request $request): Response
     {
-        return $this->render('mission/create.html.twig');
+        $mission = new Mission();
+        $form = $this->createForm(MissionType::class, $mission);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $mission = $form->getData();
+            $this->missionRepository->save($mission);
+
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->renderForm('mission/create.html.twig', [
+            'form' => $form
+        ]);
     }
+
 
     /**
      * @Route ("/missions/{id<[0-9]+>}", name= "app_mission_show")
